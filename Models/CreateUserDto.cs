1223e3d613f2010f74816b72d10ff9a5d085c7b8
SardorSohinazarov <sardorstudent0618@gmail.com>
2023-01-10 18:00:06 +0500

    CreateUserDto da Name bilan Phoneni o'zini qoldirdim

--- Models/CreateUserDto.cs
+++ Models/CreateUserDto.cs
@@ -6,11 +6,6 @@ namespace IT.Models
     {
         [Required]
         public string? Name { get; set; }
-
-        public string? Email { get; set; }
-        [Required]
-        public string? Password { get; set; }
-        public string? ConfirmPassword { get; set; }
         [Required]
         public int Phone { get; set; }
 
